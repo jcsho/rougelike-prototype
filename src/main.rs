b20ebc2022ee96b5e9c4a52e10ccceafb5facc3f
Justin Ho <justinhodev@gmail.com>
2021-03-14 21:21:23 -0700

    feat: rltk walking a map

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,9 @@ impl GameState for State {
 
         player_input(self, ctx);
         self.run_systems();
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
@@ -64,13 +67,88 @@ impl State {
     }
 }
 
+#[derive(PartialEq, Clone, Copy)]
+enum TileType {
+    Wall,
+    Floor,
+}
+
+fn xy_index(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80 * 50];
+
+    for x in 0..80 {
+        map[xy_index(x, 0)] = TileType::Wall;
+        map[xy_index(x, 49)] = TileType::Wall;
+    }
+
+    for y in 0..50 {
+        map[xy_index(0, y)] = TileType::Wall;
+        map[xy_index(79, y)] = TileType::Wall;
+    }
+
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let index = xy_index(x, y);
+        if index != xy_index(40, 25) {
+            map[index] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut x = 0;
+    let mut y = 0;
+
+    for tile in map.iter() {
+        match tile {
+            TileType::Floor => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.5, 0.5, 0.5),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('.'),
+                );
+            }
+            TileType::Wall => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.0, 1.0, 0.0),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('#'),
+                );
+            }
+        }
+
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destination_index = xy_index(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_index] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
@@ -98,6 +176,8 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
+
+    gs.ecs.insert(new_map());
 
     gs.ecs
         .create_entity()
