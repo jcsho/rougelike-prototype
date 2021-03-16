4a67eaf9a54a18ffb66d376e4da490dcc238296a
Justin Ho <github@jcsh.dev>
2021-03-16 00:34:26 -0700

    feat: rltk field of view

---
+++ src/visibility_system.rs
@@ -0,0 +1,43 @@
+use super::{Map, Player, Position, ViewShed};
+use rltk::{field_of_view, Point};
+use specs::prelude::*;
+
+pub struct VisibilitySystem {}
+
+impl<'a> System<'a> for VisibilitySystem {
+    type SystemData = (
+        WriteExpect<'a, Map>,
+        Entities<'a>,
+        WriteStorage<'a, ViewShed>,
+        WriteStorage<'a, Position>,
+        ReadStorage<'a, Player>,
+    );
+
+    fn run(&mut self, data: Self::SystemData) {
+        let (mut map, entities, mut viewshed, pos, player) = data;
+
+        for (ent, viewshed, pos) in (&entities, &mut viewshed, &pos).join() {
+            if viewshed.dirty {
+                viewshed.dirty = false;
+                viewshed.visible_tiles.clear();
+                viewshed.visible_tiles =
+                    field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
+                viewshed
+                    .visible_tiles
+                    .retain(|p| p.x >= 0 && p.x < map.width && p.y >= 0 && p.y < map.height);
+
+                let p: Option<&Player> = player.get(ent);
+                if let Some(_p) = p {
+                    for t in map.visible_tiles.iter_mut() {
+                        *t = false
+                    }
+                    for vis in viewshed.visible_tiles.iter() {
+                        let index = map.xy_index(vis.x, vis.y);
+                        map.revealed_tiles[index] = true;
+                        map.visible_tiles[index] = true;
+                    }
+                }
+            }
+        }
+    }
+}
